ce64cddf44c5d9efc7bb7c4aeccca12d1324de56
Xuan Phuc <dev.xuanphuc@gmail.com>
2022-09-30 11:23:05 +0700

    fixed UI

--- src/components/ChatRoom/Sidebar/index.js
+++ src/components/ChatRoom/Sidebar/index.js
@@ -1,14 +1,19 @@
 import classNames from "classnames/bind";
 import styles from "./Sidebar.module.scss";
 
+import { useContext } from "react";
+import { AppContext } from "../../../Context/AppProvider";
+
 import UserInfo from "../UserInfo";
 import RoomList from "../RoomList";
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
+  const { theme } = useContext(AppContext);
+
   return (
-    <div className={cx("side-bar")}>
+    <div className={cx("side-bar")} data-theme={theme}>
       <UserInfo />
       <RoomList />
     </div>
