97da948fc88b80750fe0e57f9d1d8afb579a14b4
Xuan Phuc <dev.xuanphuc@gmail.com>
2022-10-03 22:58:31 +0700

    update UI

--- src/components/ChatRoom/Sidebar/index.js
+++ src/components/ChatRoom/Sidebar/index.js
@@ -25,6 +25,7 @@ function Sidebar() {
       <AlertModal />
 
       <UserOptions />
+
       {/* Create Room Modal */}
       <CreateRoomModal />
     </div>
--- src/components/ChatRoom/UserInfo/index.js
+++ src/components/ChatRoom/UserInfo/index.js
@@ -4,7 +4,7 @@ import styles from "./UserInfo.module.scss";
 import { useContext } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import { AppContext } from "../../../Context/AppProvider";
 import userPlacehoderImg from "../../../assets/images/user.png";
@@ -23,27 +23,42 @@ function UserInfo() {
 
   return (
     <div className={cx("user-info", { fixed: isMobile })}>
-      <div
-        onClick={() => {
-          setUserOptionsVisible(true);
-        }}
-        className={cx("container")}
-      >
-        {currentUser && (
-          <img
-            className={cx("user-img")}
-            src={currentUser.photoURL || userPlacehoderImg}
-            alt=""
-          />
-        )}
+      <div className={cx("user-info-wrapper")}>
+        <div
+          onClick={() => {
+            setUserOptionsVisible(true);
+          }}
+          className={cx("container")}
+        >
+          {currentUser && (
+            <img
+              className={cx("user-img")}
+              src={currentUser.photoURL || userPlacehoderImg}
+              alt=""
+            />
+          )}
+        </div>
+
+        <h2 className={cx("title")}>Chat</h2>
+
+        <div className={cx("new-room-wrap")}>
+          <i onClick={handleAddRoom} className={cx("new-room-icon")}>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </i>
+        </div>
       </div>
 
-      <h2 className={cx("title")}>Chat</h2>
-
-      <div className={cx("new-room-wrap")}>
-        <i onClick={handleAddRoom} className={cx("new-room-icon")}>
-          <FontAwesomeIcon icon={faPenToSquare} />
-        </i>
+      <div className={cx("users-search")}>
+        <div className={cx("users-search_input-wrap")}>
+          <span className={cx("users-search_icon")}>
+            <FontAwesomeIcon icon={faSearch} />
+          </span>
+          <input
+            className={cx("users-search_input")}
+            type="text"
+            placeholder="Tìm kiếm bạn bè"
+          />
+        </div>
       </div>
     </div>
   );
@@ -54,14 +69,23 @@ const Loading = () => {
 
   return (
     <div className={cx("user-info", { fixed: isMobile })}>
-      <div className={cx("container")}>
-        <Skeleton className={cx("user-img")} />
+      <div className={cx("user-info-wrapper")}>
+        <div className={cx("container")}>
+          <Skeleton className={cx("user-img")} />
+        </div>
+
+        <Skeleton style={{ height: 18, width: 60 }} className={cx("title")} />
+
+        <div className={cx("new-room-wrap")}>
+          <Skeleton className={cx("new-room-icon")} />
+        </div>
       </div>
 
-      <Skeleton style={{ height: 18, width: 60 }} className={cx("title")} />
-
-      <div className={cx("new-room-wrap")}>
-        <Skeleton className={cx("new-room-icon")} />
+      <div className={cx("users-search")}>
+        <Skeleton
+          style={{ height: 36 }}
+          className={cx("users-search_input-wrap")}
+        />
       </div>
     </div>
   );
